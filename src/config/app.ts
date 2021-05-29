c25cfa2d5fa55ed87ebab42e4c6d0c2a75f798c9
kshumach <a.kevin.schumacher@gmail.com>
2021-05-29 01:12:58 -0400

    Remove port

--- src/config/app.ts
+++ src/config/app.ts
@@ -4,7 +4,7 @@ export interface AppConfig {
 
 const appConfig: AppConfig = {
   API_URL:
-    process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://league-in-houses.herokuapp.com:8000',
+    process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'https://league-in-houses.herokuapp.com',
 };
 
 export default appConfig;
